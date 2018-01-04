cf83aa3ffe3e43b3aa2d9260059bf19183b0c2ef
s961744 <A0012272@cppcb.com.tw>
2018-01-04 14:58:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,44 +1,82 @@
 const express = require('express'), // express 琜 
     crypto = require('crypto'), // ま盞家?
     WXBizMsgCrypt = require('wechat-crypto'), // ま盞家?
-    config = require('./config'); // ま皌竚ゅン
+    config = require('./config'), // ま皌竚ゅン
+    accessTokenJson = require('./accessToken'), //まセ? access_token
+    http = require("http"),
+    util = require('util');
 
 var app = express();// ?ㄒexpress琜
 
 // ノ?瞶┮Τ?狠 get ?钡?―
 app.get('/', function (req, res) {
-    // 1.?稬獺狝?竟Get?―?? signaturetimestampnonceechostr
-    //var signature = req.query.msg_signature,// 稬獺盞?
-    //    timestamp = req.query.timestamp,// ??耊
-    //    nonce = req.query.nonce,// ?审?
-    //    echostr = req.query.echostr;// ?审才﹃
-
-    //// 2.?tokentimestampnonce????︽ㄥ逼
-    //var array = [config.token, timestamp, nonce];
-    //array.sort();
-
-    //// 3.????才﹃钡Θ?才﹃?︽sha1盞
-    //var tempStr = array.join('');
-    //const hashCode = crypto.createHash('sha1'); // ?盞? 
-    //var resultCode = hashCode.update(tempStr, 'utf8').digest('hex'); // ??才﹃?︽盞
-
-    //// 4.???眔盞才﹃蒓signature?ゑ????―?方稬獺
-    //if (resultCode === signature) {
-    //    res.send(echostr);
-    //} else {
-    //    res.send("resultCode=" + resultCode + ",signature=" + signature +
-    //        ",config.token=" + config.token + ",timestamp=" + timestamp +
-    //        ",nonce=" + nonce + ",echostr=" + echostr);
-    //}
-    //res.send("123456789");
     var msg_signature = req.query.msg_signature;
     var timestamp = req.query.timestamp;
     var nonce = req.query.nonce;
     var echostr = req.query.echostr;
     var cryptor = new WXBizMsgCrypt(config.token, config.encodingAESKey, config.corpId)
     var s = cryptor.decrypt(echostr);
-    res.send(s.message);
+    //res.send(s.message);
+
+    getAccessToken().then(function (data) {
+        res.send(data);
+    });
  });
+
+//ノ?瞶 https Get?―よ猭
+this.requestGet = function (url) {
+    return new Promise(function (resolve, reject) {
+        https.get(url, function (res) {
+            var buffer = [], result = "";
+            //? data ㄆン
+            res.on('data', function (data) {
+                buffer.push(data);
+            });
+            //? ?誹??ЧΘㄆン
+            res.on('end', function () {
+                result = Buffer.concat(buffer, buffer.length).toString('utf-8');
+                //?程?狦
+                resolve(result);
+            });
+        }).on('error', function (err) {
+            reject(err);
+        });
+    });
+}
+
+/**
+ * ?稬獺 access_token
+ */
+function getAccessToken() {
+    var that = this;
+    return new Promise(function (resolve, reject) {
+        //??玡?? 
+        var currentTime = new Date().getTime();
+        //Αて?―
+        var url = util.format(config.accessTokenApiURL.accessTokenApi, config.corpId, config.corpSecret);
+        console.log(url);
+        //? セ? access_token 琌Τ
+        if (accessTokenJson.access_token === "" || accessTokenJson.expires_time < currentTime) {
+            that.requestGet(url).then(function (data) {
+                var result = JSON.parse(data);
+                if (data.indexOf("errcode") < 0) {
+                    accessTokenJson.access_token = result.access_token;
+                    accessTokenJson.expires_time = new Date().getTime() + (parseInt(result.expires_in) - 200) * 1000;
+                    //穝セ?
+                    fs.writeFile('./wechat/access_token.json', JSON.stringify(accessTokenJson));
+                    //?? access_token 
+                    resolve(accessTokenJson.access_token);
+                } else {
+                    //???
+                    resolve(result);
+                }
+            });
+        } else {
+            //?セ? access_token 
+            resolve(accessTokenJson.access_token);
+        }
+    });
+}
 
 //  express 箇砞ǐ port 3000τ heroku 箇砞玱ぃ琌璶硓筁祘Α锣传
 var server = app.listen(process.env.PORT || 443, function () {
