edcfb2794faecbd3d3611d9883d06420f8d5e35f
s961744 <A0012272@cppcb.com.tw>
2018-01-11 18:29:24 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ var job = schedule.scheduleJob('5,35 * * * * *', function () {
                             try {
                                 var post_data = JSON.parse(JSON.stringify('{"touser": "' + user_id + '", "msgtype": "text", "agentid": 1000002,"text" : {"content" : "' + message + '"},"safe": 0}'));
                                 console.log("post_data=" + JSON.stringify(post_data));
-                                wechatApp.getAccessToken("directory", process.env.agentSecret1000002).then(function (data) {
+                                wechatApp.getAccessToken("directory", process.env.directorySecret).then(function (data) {
                                     wechatApp.postMsg(post_data).then(function () {
                                         // 設定PUT RESTful API連接參數
                                         var paraPut = '?strMessageId=' + message_id;
