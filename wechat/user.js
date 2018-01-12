ce68432ed23564d6316f17a8d56a98d22b31c71b
s961744 <A0012272@cppcb.com.tw>
2018-01-12 16:42:57 +0800

    Add files via upload

--- wechat/user.js
+++ wechat/user.js
@@ -3,10 +3,8 @@
 const
     https = require('https'),
     util = require('util'),
-    WeChat = require('./wechat').WeChat,
+    wechatApp = require('./wechat'),
     config = require('./../config');
-
-var wechatApp = new WeChat(config);
 
 /**
  * 讀取成員
