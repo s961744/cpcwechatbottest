b1e705205a44c9cdc2ae2839aa5d54d5c8fcaf18
s961744 <A0012272@cppcb.com.tw>
2018-01-12 13:19:15 +0800

    Add files via upload

--- wechat/wechat.js
+++ wechat/wechat.js
@@ -195,9 +195,8 @@ WeChat.prototype.getAccessToken = function (secretType,secret){
                             resolve(result);
                         }
                     });
-                    //尚未過期，直接返回    
+                //尚未過期，直接返回    
                 } else {
-                    //将本地存储的 access_token 返回
                     resolve(accessTokenJson.agent1000002.access_token);
                 }
                 break;
@@ -209,21 +208,19 @@ WeChat.prototype.getAccessToken = function (secretType,secret){
 }
 
 /**
- * 微信消息处理
- * @param {Request} req Request 对象
- * @param {Response} res Response 对象
+ * 微信訊息接收處理
+ * @param {Request} req Request
+ * @param {Response} res Response
  */
 WeChat.prototype.handleMsg = function(req,res){
     var buffer = [],that = this;
-
-    //实例微信消息加解密
     var cryptoGraphy = new CryptoGraphy(that.config,req);
 
-    //监听 data 事件 用于接收数据
+    //接收資料
     req.on('data',function(data){
         buffer.push(data);
     });
-    //监听 end 事件 用于处理接收完成的数据
+    //處理接收完成的資料
     req.on('end',function(){
         var msgXml = Buffer.concat(buffer).toString('utf-8');
 
@@ -232,63 +229,59 @@ WeChat.prototype.handleMsg = function(req,res){
             console.log("result=" + JSON.stringify(result));
             if(!err){
                 result = result.xml;
-                //判断消息加解密方式
+                //消息解密
                 result = cryptoGraphy.decryptMsg(result.Encrypt);
-                var toUser = result.ToUserName; //接收方微信
-                var fromUser = result.FromUserName;//发送仿微信
-                var reportMsg = ""; //声明回复消息的变量
+                var toUser = result.ToUserName; //接收方
+                var fromUser = result.FromUserName;//發送方
+                var reportMsg = ""; 
 
-                //判断消息类型
+                //判斷訊息類型
+                //事件
                 if(result.MsgType.toLowerCase() === "event"){
-                    //判断事件类型
-                    switch(result.Event.toLowerCase()){
+                    //判斷事件類型
+                    switch (result.Event.toLowerCase()) {
+                        //成員加入
                         case 'subscribe':
-                            //回复消息
-                            var content = "欢迎关注 hvkcoder 公众号，一起斗图吧。回复以下数字：\n";
-                                content += "1.你是谁\n";
-                                content += "2.关于Node.js\n";
-                                content += "回复 “文章”  可以得到图文推送哦~\n";
+                            var content = "您好，敬鵬微信推播Bot歡迎您\n";
                             reportMsg = msg.txtMsg(fromUser,toUser,content);
-                        break;
+                            break;
+                        //選單按鈕
                         case 'click':
                              var contentArr = [
                                 {Title:"Node.js 微信自定义菜单",Description:"使用Node.js实现自定义微信菜单",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72868520"},
                                 {Title:"Node.js access_token的获取、存储及更新",Description:"Node.js access_token的获取、存储及更新",PicUrl:"http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72783631"},
                                 {Title:"Node.js 接入微信公众平台开发",Description:"Node.js 接入微信公众平台开发",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72765279"}
                             ];
-                            //回复图文消息
                             reportMsg = msg.graphicMsg(fromUser,toUser,contentArr);
                         break;
                     }
-                }else{
-                     //判断消息类型为 文本消息
-                    if(result.MsgType.toLowerCase() === "text"){
-                        //根据消息内容返回消息信息
-                        switch(result.Content){
-                            case '1':
-                                reportMsg = msg.txtMsg(fromUser,toUser,'Hello ！我的英文名字叫 H-VK');
+                }
+                //文字
+                else if(result.MsgType.toLowerCase() === "text"){
+                    //根據使用者輸入訊息回傳值
+                    switch (result.Content) {
+                        case '文字':
+                            var content = "Echo:" + result.Content;
+                            reportMsg = msg.txtMsg(fromUser, toUser, Content);
                             break;
-                            case '2':
-                                reportMsg = msg.txtMsg(fromUser,toUser,'Node.js是一个开放源代码、跨平台的JavaScript语言运行环境，采用Google开发的V8运行代码,使用事件驱动、非阻塞和异步输入输出模型等技术来提高性能，可优化应用程序的传输量和规模。这些技术通常用于数据密集的事实应用程序');
+                        case '圖文選單':
+                            var contentArr = [
+                                { Title: "Node.js 微信自定义菜单", Description: "使用Node.js实现自定义微信菜单", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72868520" },
+                                { Title: "Node.js access_token的获取、存储及更新", Description: "Node.js access_token的获取、存储及更新", PicUrl: "http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72783631" },
+                                { Title: "Node.js 接入微信公众平台开发", Description: "Node.js 接入微信公众平台开发", PicUrl: "http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast", Url: "http://blog.csdn.net/hvkcoder/article/details/72765279" }
+                            ];
+                            reportMsg = msg.graphicMsg(fromUser, toUser, contentArr);
                             break;
-                            case '文章':
-                                var contentArr = [
-                                    {Title:"Node.js 微信自定义菜单",Description:"使用Node.js实现自定义微信菜单",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72868520"},
-                                    {Title:"Node.js access_token的获取、存储及更新",Description:"Node.js access_token的获取、存储及更新",PicUrl:"http://img.blog.csdn.net/20170528151333883?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72783631"},
-                                    {Title:"Node.js 接入微信公众平台开发",Description:"Node.js 接入微信公众平台开发",PicUrl:"http://img.blog.csdn.net/20170605162832842?watermark/2/text/aHR0cDovL2Jsb2cuY3Nkbi5uZXQvaHZrQ29kZXI=/font/5a6L5L2T/fontsize/400/fill/I0JBQkFCMA==/dissolve/70/gravity/SouthEast",Url:"http://blog.csdn.net/hvkcoder/article/details/72765279"}
-                                ];
-                                //回复图文消息
-                                reportMsg = msg.graphicMsg(fromUser,toUser,contentArr);
+                        default:
+                            var content = "無此指令!\n";
+                            content += "請輸入正確指令或洽台灣資訊處(#1409)";;
+                            reportMsg = msg.txtMsg(fromUser, toUser, content);
                             break;
-                            default:
-                                reportMsg = msg.txtMsg(fromUser,toUser,'没有这个选项哦');
-                            break;
-                        }
                     }
                 }
-                //判断消息加解密方式，如果未加密则使用明文，对明文消息进行加密
+                //回復訊息加密
                 reportMsg = cryptoGraphy.encryptMsg(reportMsg);
-                //返回给微信服务器
+                //返回给微信伺服器
                 res.send(reportMsg);
 
             }else{
